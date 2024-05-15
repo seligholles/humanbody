f240a4a5486f6773f754e0d6308accd53f0e9288
seligholles <selig.holles@gmail.com>
2024-05-15 18:00:45 +0300

    made the num column nullable

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -3,7 +3,11 @@ select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
 union select 'mouth', 2
+union select 'teeth', 32
 union select 'toungue', 1
+
+insert body(bodypart)
+select 'hair'
 
 
 
